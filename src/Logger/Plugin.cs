bd063d1633730fad59ef19903d7a07a1cb1190b2
meishu <mfong@umail.ucsb.edu>
2014-02-06 18:44:01 -0800

    finish implementing basic logger

--- src/Logger/Plugin.cs
+++ src/Logger/Plugin.cs
@@ -1,17 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 using TeraWatcherAPI;
 using TeraWatcherAPI.Events;
 
 namespace Logger {
 	class Plugin : IPlugin {
+		private Stopwatch Timer;
+
+		public Plugin() {
+			Timer = new Stopwatch();
+			Timer.Start();
+		}
+
 		public void Load(IHandler Handler) {
-			Handler.sPlayerInfo += delegate(sPlayerInfoArgs e) {
-				Handler.Log(2, "sPlayerInfo");
+			Handler.sPacket += OnPacket(Handler, true);
+			Handler.cPacket += OnPacket(Handler, false);
+		}
+
+		private gPacketHandler OnPacket(IHandler Handler, bool fromServer) {
+			return delegate(gPacketArgs packet) {
+				Handler.Log(2, "{0} {1} | {2} {3:X4} ({4,4}) | {5}",
+					(packet.unknown ? ' ' : '*'),
+					Timer.ElapsedMilliseconds,
+					(fromServer ? "<-" : "->"),
+					BitConverter.ToUInt16(packet.data, 2),
+					BitConverter.ToUInt16(packet.data, 0),
+					BitConverter.ToString(packet.data).Replace('-', ' ')
+				);
 			};
 		}
 	}
